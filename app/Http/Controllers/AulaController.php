b45f12c0bf6ce4ca73bfb18016c5eecea6fe9d04
JoaoRob-Dev <joaorob.dev@gmail.com>
2023-10-16 20:27:31 -0300

    first commit

---
+++ app/Http/Controllers/AulaController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AulaController extends Controller
+{
+    public function mostrarFormAula(){
+        return view('cad_aula');
+    }
+}
